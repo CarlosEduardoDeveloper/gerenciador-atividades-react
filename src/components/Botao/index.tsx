783c626bdf2fd0962d957c903edff675e4aaead2
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2023-10-26 18:38:43 -0300

    second commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './Botao.module.scss'
+
+class Botao extends React.Component<any, any> {
+    render() {
+        const {type = "button "} = this.props;
+        return (
+            <button type={type} className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
+
+/* {
+    type?: "button" | "submit" | "reset" | undefined
+} */
